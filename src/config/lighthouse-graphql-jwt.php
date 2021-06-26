892fc3131b0ca4e5f0e0f8bdfb3d2e7dfd805a4b
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-06-27 07:08:57 +0900

    [update] lighthouse-graphql-jwt 導入

---
+++ src/config/lighthouse-graphql-jwt.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | GraphQL schema
+    |--------------------------------------------------------------------------
+    |
+    | File path of the GraphQL schema to be used, defaults to null so it uses
+    | the default location
+    |
+    */
+    'schema' => base_path('graphql/auth.graphql'),
+    /*
+    |--------------------------------------------------------------------------
+    | Settings for email verification
+    |--------------------------------------------------------------------------
+    |
+    | Update this values for your use case
+    |
+    */
+    'verify_email' => [
+        'base_url' => env('APP_URL').'/email-verify',
+    ],
+];
