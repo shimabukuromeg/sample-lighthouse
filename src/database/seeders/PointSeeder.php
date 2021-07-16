25e164ae17209e34cd9dd8c259d0b98ba9cd0441
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-07-16 13:01:37 +0900

    [update] POINT関連のModelとテーブル追加した

---
+++ src/database/seeders/PointSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Point;
+use Illuminate\Database\Seeder;
+
+class PointSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Point::truncate();
+        for ($i = 1; $i < 5; $i++) {
+            Point::factory()->create([
+                'point' => $i * 1000,
+                'price' => $i * 1000,
+            ]);
+        }
+    }
+}
